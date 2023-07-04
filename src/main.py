2ec8a39b9f2d91dc31e942aef8e0648f08a9a6cc
SokvisalMong <smong@paragoniu.edu.kh>
2023-07-04 22:55:38 +0700

    added a simple gui

--- src/main.py
+++ src/main.py
@@ -1,29 +1,26 @@
-import numpy as np
-import pickle5 as pickle
-import keras
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.models import Sequential
+import tkinter as tk
+from analyze import predict_sentiment
 
-model = keras.models.load_model('./models/review_anal_model_v2.h5')
-with open('./src/tokenizer.pickle', 'rb') as handle:
-    tokenizer = pickle.load(handle)
-
-def predict_sentiment(text):
-    # Tokenize and pad the input text
-    text_sequence = tokenizer.texts_to_sequences([text])
-    text_sequence = pad_sequences(text_sequence, maxlen=100)
-
-    # Make a prediction using the trained model
-    predicted_rating = model.predict(text_sequence)[0]
-    if np.argmax(predicted_rating) == 0:
-        return 'Negative' + str(predicted_rating)
-    else:
-        return 'Positive' + str(predicted_rating)
-    
-if __name__ == "__main__":
-    ## Replace this string here
-    text_input = "It was ok"
-    
+def analyze_text():
+    text_input = input_text.get("1.0", "end-1c")
     predicted_sentiment = predict_sentiment(text_input)
-    print(predicted_sentiment)
+    sentiment_label.config(text=predicted_sentiment)
+
+# Create the main window
+window = tk.Tk()
+window.title("Sentiment Analysis")
+
+# Create the input text field
+input_text = tk.Text(window, height=10, width=50, padx= 50, pady=50)
+input_text.pack()
+
+# Create the analyze button
+analyze_button = tk.Button(window, text="Analyze", command=analyze_text)
+analyze_button.pack()
+
+# Create the label for displaying the sentiment
+sentiment_label = tk.Label(window, text="")
+sentiment_label.pack()
+
+# Start the main event loop
+window.mainloop()
